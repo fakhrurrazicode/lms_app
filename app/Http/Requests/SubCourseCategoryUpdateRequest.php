8580a7eba02f65075be5def85c6a0701cf8afb87
fakhrurrazi.code <fakhrurrazi.code@gmail.com>
2025-01-12 16:03:21 +0700

    Sub Category module fixing error

--- app/Http/Requests/SubCourseCategoryUpdateRequest.php
+++ app/Http/Requests/SubCourseCategoryUpdateRequest.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Requests;
 
+use App\Models\CourseCategory;
+use App\Models\SubCourseCategory;
 use Illuminate\Foundation\Http\FormRequest;
 
 class SubCourseCategoryUpdateRequest extends FormRequest
@@ -23,8 +25,8 @@ class SubCourseCategoryUpdateRequest extends FormRequest
     {
         return [
             'course_category_id' => ['required', 'exists:' . CourseCategory::class . ',id'],
-            'name' => ['required', 'max:50', 'unique:' . SubCourseCategory::class . ',name'],
-            'slug' => ['required', 'max:50', 'unique:' . SubCourseCategory::class . ',slug'],
+            'name' => ['required', 'max:50', 'unique:' . SubCourseCategory::class . ',name,' . $this->route('sub_course_category')->id],
+            'slug' => ['required', 'max:50', 'unique:' . SubCourseCategory::class . ',slug,' . $this->route('sub_course_category')->id],
         ];
     }
 }
