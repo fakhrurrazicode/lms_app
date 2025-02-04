4f6cfad19d12ee891692ff9552746665c08753de
fakhrurrazicode <fakhrurrazi.code@gmail.com>
2025-02-04 09:33:42 +0700

    Adding username on authentication

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+
+use Illuminate\Auth\MustVerifyEmail;
 use Spatie\Activitylog\LogOptions;
 use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
@@ -13,7 +15,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasRoles, LogsActivity;
+    use HasFactory, Notifiable, HasRoles, LogsActivity, MustVerifyEmail;
 
 
     public function getActivitylogOptions(): LogOptions
