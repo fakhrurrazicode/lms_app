7ba1e4494cb27d27d500ccac2dc4dc395b710ac2
Hendra Sagita <hendrabox2014@gmail.com>
2025-02-17 15:34:36 +0700

    Update Welcome

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -550,7 +550,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-12 gap-30px mb-65px">
                             <div className="lg:col-star-1 lg:col-span-4">
                                 <span className="text-sm font-semibold text-primaryColor bg-whitegrey3 px-6 py-5px mb-5 rounded-full inline-block">
-                                    Course List
+                                    Daftar Kursus
                                 </span>
                                 <h3 className="text-3xl md:text-size-35 2xl:text-size-38 3xl:text-size-42 leading-10 md:leading-45px 2xl:leading-50px 3xl:leading-2xl font-bold text-blackColor dark:text-blackColor-dark">
                                     <span className="relative after:w-full after:h-[7px] after:bg-secondaryColor after:absolute after:left-0 after:bottom-3 md:after:bottom-4 after:z-[-1]">
