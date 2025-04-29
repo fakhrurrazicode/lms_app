1067fe87d96e19904878d54ab6a503dbabdedbcb
fakhrurrazicode <fakhrurrazi.code@gmail.com>
2025-04-29 13:52:45 +0700

    Update email format/templating

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,20 @@
+{{-- @props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+@if (trim($slot) === 'Laravel')
+<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+@else
+{{ $slot }}
+@endif
+</a>
+</td>
+</tr> --}}
+
+<tr>
+    <td class="header">
+        <a href="{{ config('app.url') }}" style="display: inline-block;">
+            <img src="{{ asset('images/logo-light.svg') }}" alt="{{ config('app.name') }}" style="height: 50px;">
+        </a>
+    </td>
+</tr>
